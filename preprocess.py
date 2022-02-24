e10dfb7431c8b18f6f34614a18d66645a8f6c11c
Fangxu Yu <58104156+Yu-Fangxu@users.noreply.github.com>
2022-02-24 23:37:47 +0800

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,23 @@
-# This file aims to clean the text data
-
 import nltk
-
+from gensim.corpora import Dictionary
 nltk.download("stopwords")
-
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from nltk.corpus import stopwords
 from collections import Counter
 from tqdm import tqdm
 import re
 import joblib
 import numpy as np
-
-dataset = "mr"
+import pandas as pd
+import torch
+dataset = "ohsumed"
 
 # param
 stop_words = set(stopwords.words('english'))
-least_freq = 100
-if dataset == "mr" or "SST" in dataset:
-    # stop_words = set()
-    least_freq = 0
+least_freq = 50
+# if dataset == "mr" or "SST" in dataset:
+#     # stop_words = set()
+#     least_freq = 10
 
 
 # func load texts & labels
@@ -57,36 +56,45 @@ if __name__ == '__main__':
     # handle texts
     texts_clean = [filter_text(t) for t in texts]
 
-    word2count = Counter([w for t in texts_clean for w in t.split()])
-    word_count = [[w, c] for w, c in word2count.items() if c >= least_freq and w not in stop_words]
-    word2index = {w: i for i, (w, c) in enumerate(word_count)}
+    # word2count = Counter([w for t in texts_clean for w in t.split()])
+    # word_count = [[w, c] for w, c in word2count.items() if c >= least_freq and w not in stop_words]
+    # word2index = {w: i for i, (w, c) in enumerate(word_count)}
+    vectorizer = CountVectorizer(max_df=0.5, min_df=20, stop_words='english')#, vocabulary=word2index)
+    # index2word = {i: w for i, (w, c) in enumerate(word_count)}
+    # words_list = [[w for w in t.split() if w in word2index] for t in texts_clean]
+    # texts_remove = [" ".join(ws) for ws in words_list]
+    # print(len(word2index))
+    # # labels 2 targets
 
-    words_list = [[w for w in t.split() if w in word2index] for t in texts_clean]
-
-    texts_remove = [" ".join(ws) for ws in words_list]
-
-    # labels 2 targets
     label2index = {l: i for i, l in enumerate(set(labels))}
     targets = [label2index[l] for l in labels]
 
+    docs = torch.from_numpy(vectorizer.fit_transform(texts_clean).toarray())
+    word2index = vectorizer.vocabulary_
+    index2word = {v:k for v,k in word2index.items()}
+    print(sum(docs[1]))
+    print(docs.shape)
     # save
     with open(f"temp/{dataset}.texts.clean.txt", "w") as f:
         f.write("\n".join(texts_clean))
 
-    with open(f"temp/{dataset}.texts.remove.txt", "w") as f:
-        f.write("\n".join(texts_remove))
+    # with open(f"temp/{dataset}.texts.remove.txt", "w") as f:
+    #     f.write("\n".join(texts_remove))
 
     np.save(f"temp/{dataset}.targets.npy", targets)
     joblib.dump(word2index, f"temp/{dataset}.word2index.pkl")
+    joblib.dump(index2word, f"temp/{dataset}.index2word.pkl")
+    joblib.dump(vectorizer, f"temp/{dataset}.vectorizer.pkl")
 ########################
     word2index = joblib.load(f"temp/{dataset}.word2index.pkl")
-    with open(f"temp/{dataset}.texts.remove.txt", "r") as f:
-        texts = f.read().strip().split("\n")
+    index2word = joblib.load(f"temp/{dataset}.index2word.pkl")
+    # with open(f"temp/{dataset}.texts.remove.txt", "r") as f:
+    #     texts = f.read().strip().split("\n")
 
-    BoW = np.zeros((len(texts), len(word2index)))
-    for i, text in tqdm(enumerate(texts)):
-        words = [word2index[w] for w in text.split()]
-        for j in range(len(words)):
-            BoW[i][words[j]] += 1
+    # BoW = np.zeros((len(texts), len(word2index)))
+    # for i, text in tqdm(enumerate(texts)):
+    #     words = [word2index[w] for w in text.split()]
+    #     for j in range(len(words)):
+    #         BoW[i][words[j]] += 1
 
-    np.save(f"temp/{dataset}.BoW.npy", BoW)
+    np.save(f"temp/{dataset}.BoW.npy", docs)
